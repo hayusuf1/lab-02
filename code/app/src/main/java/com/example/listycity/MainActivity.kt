9730538205777c85cf430e9940b70048dd45ea7a
Hamse Ali Yusuf <82422858+fiindhedev@users.noreply.github.com>
2026-09-09 15:00:39 -0600

    second commit

--- code/app/src/main/java/com/example/listycity/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.listycity
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,10 +19,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -24,10 +32,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.listycity.ui.theme.ListyCityTheme
+import java.util.stream.DoubleStream.builder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,6 +52,7 @@ class MainActivity : ComponentActivity() {
                    CityListScreen(
                        cities = cityRepository.cities,
                        onAddCity = {cityRepository.addCity(it)} ,
+                       onDeleteCity = {cityRepository.deleteCity(it)},
                        modifier = Modifier.padding(innerPadding)
                    )
                 }
@@ -63,6 +75,10 @@ class CityRepository {
         _cities.add(city)
     }
 
+    fun deleteCity(city:String){
+        _cities.remove(city)
+    }
+
 }
 
 
@@ -70,53 +86,140 @@ class CityRepository {
 fun CityListScreen(
     cities:List<String>,
     onAddCity: (String) -> Unit,
+    onDeleteCity: (String) -> Unit,
     modifier: Modifier = Modifier
 
 ) {
     var newCityName by remember { mutableStateOf("") }
+    var showTextField by remember { mutableStateOf(false) }
+    var selectedCity by remember { mutableStateOf("") }
 
     Column(modifier = modifier.fillMaxSize()) {
         Row(modifier = Modifier.padding(16.dp)) {
-            OutlinedTextField(
-                value = newCityName,
-                onValueChange = {newCityName = it},
-                label = {Text("City name")},
-                modifier = Modifier.weight(1f)
-            )
 
             Spacer(modifier = Modifier.width(8.dp))
 
+            // var showDialog by remember { mutableStateOf(false) }
             Button(
                 onClick = {
-                    if (newCityName.isNotBlank()){
-                        onAddCity(newCityName)
-                        newCityName = ""
-                    }
+                    showTextField = true
                 }
-            ){
+            ) {
                 Text("Add City")
+            }
+
+
+            var deleteDialog by remember { mutableStateOf(false) }
+
+            Button(
+                onClick = {
+                    deleteDialog = true
+                }
+            ) {
+                Text("Delete City")
+            }
+
+            if(deleteDialog){
+
+                AlertDialog(
+                    onDismissRequest = {
+                        deleteDialog = false
+
+                    },
+                    title = {
+                        Text("Are you sure you wanna delete this city?")
+                    },
+                    confirmButton ={
+                        TextButton(
+                            onClick = {
+                                onDeleteCity(selectedCity)
+                                deleteDialog = false
+                            }
+
+                        ) { Text("Yes")}
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = {
+                                deleteDialog = false
+                            }
+                        ) { Text("No")}
+                    }
+                        )
+                    }
+
+
+            }
+
+
+
+
+        LazyColumn(modifier = modifier.weight(1f)) {
+            items(cities) {
+                city ->
+                CityRow(
+                    city = city,
+                    onClick = {
+                        showTextField = false
+                        Modifier.background(color = Color.Blue)
+                        selectedCity = city
+
+                    }
+                )
             }
         }
 
+        if (showTextField) {
+    Row(
+        modifier = Modifier.padding(16.dp)
+    ) {
 
-    LazyColumn(modifier = modifier.fillMaxSize()) {
-        items(cities){
-            city ->
-            CityRow(city = city)
+            OutlinedTextField(
+                value = newCityName,
+                onValueChange = { newCityName = it },
+                label = { Text("City name") },
+                modifier = Modifier.weight(1f),
+
+                )
+            Button(
+                onClick = {
+                    if (newCityName.isNotBlank() && newCityName !in cities) {
+                        onAddCity(newCityName)
+                        newCityName = ""
+                        showTextField = false
+                    }
+
+                }
+            ) { Text("Confirm") }
         }
     }
 }
 }
 
 @Composable
-fun CityRow(city:String){
-    Text(
-        text = city,
-        fontSize = 28.sp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 18.dp, vertical = 14.dp)
-    )
+fun CityRow(
+    city:String,
+    onClick : () -> Unit){
+
+
+        Text(
+            text = city,
+
+            fontSize = 28.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 18.dp, vertical = 14.dp)
+                .clickable {
+                    onClick()
+                }
+        )
+
+
+
+
+
+
+
 }
 
 
